2c5a144c7becba57a39ee7f070415593029bef8b
hangyeong-eun <az2282@naver.com>
2024-02-12 05:54:14 +0900

    영화 목록 포맷팅

--- mini_project/src/com/model/Movie.java
+++ mini_project/src/com/model/Movie.java
@@ -18,15 +18,15 @@ public class Movie {
 		this.director = director;
 	}
 
-	@Override
-	public String toString() {
-		String formattedMovieId = String.format("%-3s", movieId);
-		String formattedMovieTitle = String.format("%-23s", movieTitle);
-		String formattedRunningTime = String.format("%-7s", runningTime);
-		String formattedGrade = String.format("%-6s", grade);
-		String formattedDirector = String.format("%-10s", director);
-		return formattedMovieId + "\t" + formattedMovieTitle + "\t" + formattedRunningTime + "\t" + formattedGrade
-				+ "\t"
-				+ formattedDirector;
-	}
+//	@Override
+//	public String toString() {
+//		String formattedMovieId = String.format("%-3s", movieId);
+//		String formattedMovieTitle = String.format("%-23s", movieTitle);
+//		String formattedRunningTime = String.format("%-7s", runningTime);
+//		String formattedGrade = String.format("%-6s", grade);
+//		String formattedDirector = String.format("%-10s", director);
+//		return formattedMovieId + "\t" + formattedMovieTitle + "\t" + formattedRunningTime + "\t" + formattedGrade
+//				+ "\t"
+//				+ formattedDirector;
+//	}
 }
